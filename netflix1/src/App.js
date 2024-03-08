da1579b2224571519921e7778002e2c71a64cdf9
Mesfin Desale <147912364+messfind@users.noreply.github.com>
2024-03-08 02:35:59 -0800

    Update App.js

--- netflix1/src/App.js
+++ netflix1/src/App.js
@@ -1,10 +1,37 @@
 
+import React from "react";
+import Row from "./Row";
+import './Netflix.css'
+// import Nav from "./Nav";
+import requists from "./requist";
+import Banner from "./Banner";
+
 function App() {
   return (
     <div className="App">
-    <h1>Netflix</h1>
+ <h1 className="net">NETFLIX</h1>
+      {/* <Nav/> */}
+      <Banner/>
+      <Row title="NETFLIX ORIGINAL"
+        fetchUrl={requists.fetchNetflixOriginals}
+        isLargeRow={true} />
+      <Row title="Trending Now"
+        fetchUrl={requists.fetchTrend} />
+      <Row title="Top Rated Movies"
+        fetchUrl={requists.fetchTopratedMovies} />
+      <Row title="Action Movies"
+        fetchUrl={requists.fetchActionMovies} />
+      <Row title="Comady Movies"
+        fetchUrl={requists.fetchComadyMovies} />
+      <Row title="Horror Movies"
+        fetchUrl={requists.fetchHorrorMovies} />
+      <Row title="Romance Movies"
+        fetchUrl={requists.fetchRomanceMovies} />
+      <Row title="Documentaries"
+        fetchUrl={requists.fetchDocumentaries} />
+  
     </div>
-  );
+  )
 }
 
 export default App;
